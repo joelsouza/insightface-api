3ee52307a7aa28e8d4c3083f999fdb9fc4a18a52
Joel Souza <joel.souza@gmail.com>
2025-04-13 09:01:23 -0300

    add up endpoint

--- api.py
+++ api.py
@@ -94,6 +94,10 @@ def represent():
         # Release memory
         gc.collect()
 
+@app.route('/up', methods=['GET'])
+def up():
+    return jsonify({"status": "ok"})
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=False)
